911b912acb4bba03632891bf34eaa60bdbd60cb9
Zach <zacharyeverett.00@gmail.com>
2025-08-19 16:53:01 +1000

    imported imgui

--- run.bat
+++ run.bat
@@ -1,3 +1,19 @@
 mkdir build
-g++ src/memory.cpp src/player.cpp src/utils.cpp src/keymanager.cpp src/main.cpp -o build/main.exe
+
+g++ ^
+imgui/imgui.cpp ^
+imgui/imgui_draw.cpp ^
+imgui/backends/imgui_impl_win32.cpp ^
+imgui/backends/imgui_impl_dx11.cpp ^
+imgui/imgui_widgets.cpp ^
+imgui/imgui_tables.cpp ^
+src/memory.cpp ^
+src/player.cpp ^
+src/utils.cpp ^
+src/keymanager.cpp ^
+src/main.cpp ^
+-Iimgui ^
+-o build/main.exe ^
+-lgdi32 -ldwmapi -luser32 -ld3dcompiler
+
 .\build\main.exe
