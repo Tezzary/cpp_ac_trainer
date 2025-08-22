64345dbe12bae4dedef65ecd0469791a4ee283c6
Zach <zacharyeverett.00@gmail.com>
2025-08-22 16:33:56 +1000

    rendering window correctly

--- src/guimanager.cpp
+++ src/guimanager.cpp
@@ -6,18 +6,22 @@ void StartUI()
     glfwInit();
 
     glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+    //glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
 
-    int width  = GetSystemMetrics(SM_CXSCREEN);
-    int height = GetSystemMetrics(SM_CYSCREEN);
+    int width  = 500; // GetSystemMetrics(SM_CXSCREEN);
+    int height = 400; // GetSystemMetrics(SM_CYSCREEN);
 
     GLFWwindow* window = glfwCreateWindow(width, height, "Minimal ImGui", nullptr, nullptr);
+    glfwHideWindow(window);
 
     glfwMakeContextCurrent(window);
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -29,20 +33,28 @@ void StartUI()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Hello");
+        ImGuiWindowFlags flags = ImGuiWindowFlags_NoResize |
+                                 ImGuiWindowFlags_NoBringToFrontOnFocus |
+                                 ImGuiWindowFlags_NoCollapse;
+
+        ImGui::Begin("Hello", nullptr, flags);
         ImGui::Text("Minimal boilerplate!");
         ImGui::End();
 
         ImGui::Render();
 
         int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0,0,display_w,display_h);
+       // glfwGetFramebufferSize(window, &display_w, &display_h);
+       // glViewport(0,0,display_w,display_h);
 
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // RGBA (last param = alpha)
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+
         glfwSwapBuffers(window);
     }
 
